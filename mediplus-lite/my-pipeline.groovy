d3427f31723e4ae0daf3bf5ca2f4e9c337ea82c7
Digambark07 <karedigambar88@gmail.com>
2024-10-17 12:06:00 +0530

    --- Auto Git Commit ---

--- mediplus-lite/my-pipeline.groovy
+++ mediplus-lite/my-pipeline.groovy
@@ -1,25 +1,25 @@
-pipeline {
-    agent any
-    sages {
-        stage ('pull source code') {
-            steps {
-                git branch: 'main', url: 'https://github.com/Digambark07/my-css-pipeline.git'
-            }
-        }
+// pipeline {
+//     agent any
+//     sages {
+//         stage ('pull source code') {
+//             steps {
+//                 git branch: 'main', url: 'https://github.com/Digambark07/my-css-pipeline.git'
+//             }
+//         }
 
-        stage ('build docker image') {
-            steps {
-                sh 'docker build -t my-image digambarkare/jk-8 '
-            }
-        }
+//         stage ('build docker image') {
+//             steps {
+//                 sh 'docker build -t my-image digambarkare/jk-8 '
+//             }
+//         }
 
-        stage ('push docker image ') {
-            environment {
-                registryCredntial = 'docker-cred'
-            steps {
-                withDockerRegistry(credentialsId: 'docker-cred', url: 'https://hub.docker.com/repository/docker/digambarkare/jk-8')
-            }
-            }
-        }
-    }
-}
+//         stage ('push docker image ') {
+//             environment {
+//                 registryCredntial = 'docker-cred'
+//             steps {
+//                 withDockerRegistry(credentialsId: 'docker-cred', url: 'https://hub.docker.com/repository/docker/digambarkare/jk-8')
+//             }
+//             }
+//         }
+//     }
+// }
