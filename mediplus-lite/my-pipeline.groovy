ecb686554738820dcb33771778fd25b780530b50
Digambark07 <karedigambar88@gmail.com>
2024-10-17 14:27:41 +0530

    --- Auto Git Commit ---

--- mediplus-lite/my-pipeline.groovy
+++ mediplus-lite/my-pipeline.groovy
@@ -36,7 +36,7 @@ pipeline {
 
         stage('Build Docker Image') {  // Corrected spacing
             steps {
-                sh 'docker build -t digambarkare/jk-8 ./mediplus'  // Corrected docker build command syntax
+                sh 'docker build -t digambarkare/jk-8 ./mediplus-lite/.'  // Corrected docker build command syntax
             }
         }
 
